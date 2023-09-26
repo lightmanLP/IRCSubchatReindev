07e422ec857907296a73e917edc9dd1dfddda12b
lightmanLP <liteman1000@gmail.com>
2023-09-26 19:09:08 +0700

    init

---
+++ src/client/java/com/lightmanlp/ircsubchat/IRCSubchatClient.java
@@ -0,0 +1,11 @@
+package com.lightmanlp.ircsubchat;
+
+import com.fox2code.foxloader.loader.Mod;
+import com.fox2code.foxloader.loader.ClientMod;
+
+public class IRCSubchatClient extends Mod implements ClientMod {
+    @Override
+    public void onInit() {
+        System.out.println("IRC SUBCHAT AYOOO");
+    }
+}
---
+++ src/client/java/com/lightmanlp/ircsubchat/client/mixins/MixinEntityPlayerSP.java
@@ -0,0 +1,21 @@
+package com.lightmanlp.ircsubchat.client.mixins;
+
+import net.minecraft.src.client.player.EntityPlayerSP;
+import net.minecraft.src.game.entity.player.EntityPlayer;
+import net.minecraft.src.game.level.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityPlayerSP.class)
+public abstract class MixinEntityPlayerSP extends EntityPlayer {
+    public MixinEntityPlayerSP(World world) {
+        super(world);
+    }
+
+    @Inject(method = "sendChatMessage", at = @At(value = "HEAD"))
+    public void sendChatMessageMixin(String s, CallbackInfo ci) {
+        System.out.println("[IRC] " + s);
+    }
+}
